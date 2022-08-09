b2b8bf3a9dc88871a1fcde4c45c53dfb65b9b0b6
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2022-08-09 15:12:06 +0200

    add get station data

--- codes/urls.py
+++ codes/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import GetStationData
 
-urlpatterns = []
+urlpatterns = [
+    path('station/<int:station_number>/', GetStationData.as_view())
+]
--- codes/views.py
+++ codes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Data
+from django.http import Http404
+from user_account.models import Station
+from .serializers import DataSerializer
 
 # Create your views here.
+class GetStationData(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(pk):
+        try:
+            return Station.objects.get(id=pk)
+        except:
+            raise Http404
+
+    def get(self, request, station_number):
+        user = request.user
+        station = self.get_object(station_number)
+        if user.station != station.id:
+            return Response({'message':'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        station_data = Station.objects.filter(station = station.id)
+        serializer = DataSerializer(station_data, many=True)
+        return Response(serializer.data)
