e1e8fc00d32df7ced20971d7e79f76ed03203572
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2022-08-09 16:16:20 +0200

    edit + submit data

--- codes/urls.py
+++ codes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import GetStationData
+from .views import GetStationData, SubmitData, EditData
 
 urlpatterns = [
-    path('station/<int:station_number>/', GetStationData.as_view())
+    path('station/<int:station_number>/', GetStationData.as_view()),
+    path('submit-data/', SubmitData.as_view()),
+    path('edit-data/', EditData.as_view())
 ]
--- codes/views.py
+++ codes/views.py
@@ -1,3 +1,4 @@
+from functools import partial
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
@@ -13,7 +14,7 @@ class GetStationData(APIView):
 
     def get_object(pk):
         try:
-            return Station.objects.get(id=pk)
+            return Station.objects.get(station_number=pk)
         except:
             raise Http404
 
@@ -24,4 +25,37 @@ class GetStationData(APIView):
             return Response({'message':'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
         station_data = Station.objects.filter(station = station.id)
         serializer = DataSerializer(station_data, many=True)
+        return Response(serializer.data)
+
+
+class SubmitData(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        station = Station.objects.filter(station_number=request.data['station_number']).first()
+        serializer = DataSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(station_id=station)
+            return Response(serializer.data)
+
+
+class EditData(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(pk):
+        try:
+            return Station.objects.get(station_number=pk)
+        except:
+            raise Http404
+
+    def put(self, request):
+        user = request.user
+        station = self.get_object(request.data['station_number'])
+        code = Data.objects.get(id=request.data['data_id'])
+        if code.station != station.id:
+            return Response({'message':'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        serializer = DataSerializer(code, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
