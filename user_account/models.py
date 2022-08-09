4ca1529530b7dad16c3314c1809b18225dc341d9
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2022-08-09 12:04:26 +0200

    added name field to user model

--- user_account/models.py
+++ user_account/models.py
@@ -14,6 +14,7 @@ class Station(models.Model):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    name = models.CharField(max_length=200, null=True)
     password = models.CharField(max_length=250)
     station = models.ForeignKey(Station, on_delete=models.SET_NULL, null=True)
     is_staff = models.BooleanField(
