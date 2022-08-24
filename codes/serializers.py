32e0f1f4bff672258157a64236ab18a6bb6b6142
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2022-08-24 16:10:48 +0200

    synop serializer

--- codes/serializers.py
+++ codes/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Data
+from .models import Data, synop
+
 
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Data
+        fields = '__all__'
+
+
+class SynopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = synop
         fields = '__all__'
